d9025994b21c8e6203f9a9da72df89eea6052841
Jay Abdallah <jay.h.abdallah@gmail.com>
2014-12-16 16:54:30 -0500

    Prompt

---
+++ jscriptprog.js
@@ -0,0 +1,14 @@
+document.writeln('First site aight!');
+
+
+var b = "Some string in here."
+document.writeln(b.length); 
+
+b.length; 
+
+3 + 3 + 19 + 20; 
+
+
+confirm('Just checking to see if a prompt comes up.');
+confirm('It does! Fucking sweet dude!');
+
